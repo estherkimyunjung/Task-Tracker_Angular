5dfd2331506acf13de0f1e4dbd00048e386f0dbe
estherkimyunjung <estherkimyunjung@gmail.com>
2021-10-07 22:41:52 -0500

    add reminder and delete functions

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
-import {Task} from '../../Task'
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { Task } from '../../Task'
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 
@@ -9,7 +9,9 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./task-item.component.css']
 })
 export class TaskItemComponent implements OnInit {
-  @Input() task: Task = {text: "", day: "", reminder: false};
+  @Input() task!: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
   faTimes = faTimes;
 
   constructor() { }
@@ -17,4 +19,11 @@ export class TaskItemComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onDelete(task: Task) {
+    this.onDeleteTask.emit(task);  
+  }
+
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);  
+  }
 }
